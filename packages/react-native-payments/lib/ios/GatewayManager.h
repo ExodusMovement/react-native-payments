5cf506776e80aef450ce856caa385e73d9cf63b7
Diego Muracciole <diegomuracciole@gmail.com>
2025-10-14 19:38:03 +0200

    feat: remove stripe gateway support (#20)

--- packages/react-native-payments/lib/ios/GatewayManager.h
+++ packages/react-native-payments/lib/ios/GatewayManager.h
@@ -10,10 +10,4 @@
 - (void)createTokenWithPayment:(PKPayment *_Nonnull)payment
                     completion:(void (^_Nullable)(NSString * _Nullable token, NSError * _Nullable error))completion;
 
-// Stripe
-- (void)configureStripeGateway:(NSDictionary *_Nonnull)gatewayParameters
-            merchantIdentifier:(NSString *_Nonnull)merchantId;
-- (void)createStripeTokenWithPayment:(PKPayment *_Nonnull)payment
-                          completion:(void (^_Nullable)(NSString * _Nullable token, NSError * _Nullable error))completion;
-
 @end
--- packages/react-native-payments/lib/js/NativePayments.js
+++ packages/react-native-payments/lib/js/NativePayments.js
@@ -20,7 +20,7 @@ const NativePayments: {
   getFullWalletAndroid: string => Promise<any>
 } = {
   supportedGateways: IS_ANDROID
-    ? ['stripe'] // On Android, Payment Gateways are supported out of the gate.
+    ? [] // On Android, Payment Gateways are supported out of the gate.
     : ReactNativePayments ? ReactNativePayments.supportedGateways : [],
 
   canMakePayments(methodData: object) {
