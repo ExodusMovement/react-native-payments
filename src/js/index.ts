628bef7e21a572feb7fc5f39f5a3ea9435fb3ddf
Diego Muracciole <diegomuracciole@gmail.com>
2025-10-15 16:36:42 +0200

    feat: remove apple pay button js

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,5 @@
 import _PaymentRequest from './PaymentRequest';
-import { PKPaymentButton } from './PKPaymentButton';
 import NativePayments from './NativePayments';
 
-export const ApplePayButton = PKPaymentButton;
 export const PaymentRequest = _PaymentRequest;
 export const openPaymentSetup = NativePayments.openPaymentSetup;
